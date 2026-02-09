7b99954c4bf4e3df4c314f2c282ca7c2121e88ec
Alexander <pchp250g8@tutamail.com>
2026-02-09 15:21:57 +0300

    Добавьте файлы проекта.

---
+++ SolveLinealEquation/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a As Single, b As Single
+        Console.Clear()
+        Console.WriteLine("Input coefficients of lineal equation")
+        Single.TryParse(Console.ReadLine(), a)
+        Single.TryParse(Console.ReadLine(), b)
+        Console.WriteLine("Try to solve the equation:{0}*x+{1}=0", a, b)
+        If ((a = 0) And (b = 0)) Then
+            Console.WriteLine("The answer is any number")
+        ElseIf ((a = 0) And (b <> 0)) Then
+            Console.WriteLine("The equation has no solution")
+        Else
+            Console.WriteLine("The equation has the following solution:{0}", -b / a)
+        End If
+        Console.Read()
+    End Sub
+
+End Module
---
+++ SolveLinealEquation/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("SolveLinealEquation")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SolveLinealEquation")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("a28c9434-3546-4c63-b6b9-07db4f6797c6")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
